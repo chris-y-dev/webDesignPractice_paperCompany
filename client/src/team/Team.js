5cbd77c6cfdf547e2df15cc203efacaf5719e8f3
chrisyuen976 <chrisyuen976@gmail.com>
2022-06-29 00:18:29 +1000

    'build'

--- client/src/team/Team.js
+++ client/src/team/Team.js
@@ -17,55 +17,55 @@ function Team() {
                 <div class="container py-5">
                     <div class="row d-flex flex-row justify-content-between">
                         <div class="col d-flex justify-content-center text-center">
-                            <Profile source={"/team/ms.jpg"} name={"Michael Scott"} pos="Regional Manager"/>
+                            <Profile source={"/teamPics/ms.jpg"} name={"Michael Scott"} pos="Regional Manager"/>
                         </div>
                     </div>
                     <div class="row d-flex flex-row justify-content-between">
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/ds.jpg"} name={"Dwight S."} pos="Assistant Manager"/>
+                            <Profile source={"/teamPics/ds.jpg"} name={"Dwight S."} pos="Assistant Manager"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/jh.jpg"} name={"Jim H."} pos="Sales Expert"/>
+                            <Profile source={"/teamPics/jh.jpg"} name={"Jim H."} pos="Sales Expert"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/ab.jpg"} name={"Andy B."} pos="Sales Expert"/>
+                            <Profile source={"/teamPics/ab.jpg"} name={"Andy B."} pos="Sales Expert"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/sh.jpg"} name={"Stanley H."} pos="Sales Expert"/>
+                            <Profile source={"/teamPics/sh.jpg"} name={"Stanley H."} pos="Sales Expert"/>
                         </div>
                     </div>
                     <div class="row d-flex flex-row justify-content-between"> 
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/pv.jpg"} name={"Phyllis V."} pos="Sales Expert"/>
+                            <Profile source={"/teamPics/pv.jpg"} name={"Phyllis V."} pos="Sales Expert"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/pb.jpg"} name={"Pam B."} pos="Office Administrator"/>
+                            <Profile source={"/teamPics/pb.jpg"} name={"Pam B."} pos="Office Administrator"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/om.jpg"} name={"Oscar M."} pos="Accounting"/>
+                            <Profile source={"/teamPics/om.jpg"} name={"Oscar M."} pos="Accounting"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/am.jpg"} name={"Angela M."} pos="Accounting"/>
+                            <Profile source={"/teamPics/am.jpg"} name={"Angela M."} pos="Accounting"/>
                         </div>
                     </div>
                     <div class="row d-flex flex-row justify-content-between">
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/km.jpg"} name={"Kevin M."} pos="Accounting"/>
+                            <Profile source={"/teamPics/km.jpg"} name={"Kevin M."} pos="Accounting"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/kk.jpg"} name={"Kelly K."} pos="Customer Service"/>
+                            <Profile source={"/teamPics/kk.jpg"} name={"Kelly K."} pos="Customer Service"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/mp.jpg"} name={"Meredith P."} pos="Supplier Relationships"/>
+                            <Profile source={"/teamPics/mp.jpg"} name={"Meredith P."} pos="Supplier Relationships"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/cb.jpg"} name={"Creed B."} pos="Quality Assurance"/>
+                            <Profile source={"/teamPics/cb.jpg"} name={"Creed B."} pos="Quality Assurance"/>
                         </div>
                     </div>
                     
                     <div class="row d-flex flex-row justify-content-between">
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-center text-center">
-                            <Profile source={"/team/dp.jpg"} name={"Darryl P."} pos="Shipping/Distribution"/>
+                            <Profile source={"/teamPics/dp.jpg"} name={"Darryl P."} pos="Shipping/Distribution"/>
                         </div>
                         <div class="col-sm-6 col-lg-3 d-flex justify-content-between text-center">
                         </div>
